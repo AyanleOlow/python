6f6000f9fad482acea0f3a5cca0875aa7eafc5d0
AyanelOlow <randomrandomo156@gmail.com>
2025-09-16 10:08:45 +0200

    hellow world

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-def første_avsnitt():
-    print("Her starter vår historie...")
-def andre_avsnitt():
- pass # Blir laget senere
-def tredje_avsnitt():
-    pass # Blir laget senere
-def print_ut_historien():
-    første_avsnitt()
-    andre_avsnitt()
-    tredje_avsnitt()
-print_ut_historien()
- ###
+print("Hello, World!")
